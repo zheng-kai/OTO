64b4827168cacafe6083f664624521d6d741a47c
zk <272991962@qq.com>
2019-08-10 09:59:37 +0800

    运输首页UI

--- app/src/main/java/com/app/oto/otoapplication/transport/TransportHome.kt
+++ app/src/main/java/com/app/oto/otoapplication/transport/TransportHome.kt
@@ -1,13 +1,13 @@
-package com.app.oto.otoapplication
+package com.app.oto.otoapplication.transport
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.res.ResourcesCompat
+import android.support.v7.app.AppCompatActivity
+import com.app.oto.otoapplication.R
 import kotlinx.android.synthetic.main.home_activity_transport.*
 import kotlinx.android.synthetic.main.navigation_layout.view.*
 
-class OTOActivity : AppCompatActivity() {
-
+class TransportHome :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity_transport)
