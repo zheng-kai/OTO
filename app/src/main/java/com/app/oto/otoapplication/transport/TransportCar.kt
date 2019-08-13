c3efcc72736323f89825bce0ade30dc781905e83
zk <272991962@qq.com>
2019-08-13 20:44:13 +0800

    添加列表

--- app/src/main/java/com/app/oto/otoapplication/transport/TransportCar.kt
+++ app/src/main/java/com/app/oto/otoapplication/transport/TransportCar.kt
@@ -3,6 +3,8 @@ package com.app.oto.otoapplication.transport
 import android.os.Bundle
 import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import cn.edu.twt.retrox.recyclerviewdsl.withItems
 import com.app.oto.otoapplication.R
 import kotlinx.android.synthetic.main.navigation_layout.view.*
 import kotlinx.android.synthetic.main.transport_car.*
@@ -16,6 +18,12 @@ class TransportCar:AppCompatActivity() {
         }
         transport_car_navigation.apply {
             btn_transport.background = ResourcesCompat.getDrawable(resources,R.mipmap.transport_clicked,null)
+        }
+        rec_transport_car.layoutManager = LinearLayoutManager(this)
+        rec_transport_car.withItems {
+            for(i in 1..5){
+                add("xxxxx","A","$i 元")
+            }
         }
     }
 
