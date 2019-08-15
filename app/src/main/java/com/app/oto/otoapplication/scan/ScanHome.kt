c982f3b75b26089e4b4ad4cf6e4b2c493ee3ad83
zk <272991962@qq.com>
2019-08-15 10:53:00 +0800

    这辣鸡扫码库

--- app/src/main/java/com/app/oto/otoapplication/scan/ScanHome.kt
+++ app/src/main/java/com/app/oto/otoapplication/scan/ScanHome.kt
@@ -1,5 +1,6 @@
 package com.app.oto.otoapplication.scan
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
@@ -7,20 +8,18 @@ import cn.bingoogolapple.qrcode.core.QRCodeView
 import com.app.oto.otoapplication.R
 import kotlinx.android.synthetic.main.home_activity_scan.*
 import kotlinx.android.synthetic.main.navigation_layout.view.*
+import android.os.Vibrator
+import android.support.v4.app.FragmentActivity
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
+import cn.bingoogolapple.qrcode.core.BarcodeType
+import com.app.oto.otoapplication.commons.CommonContext
+import java.security.Permission
+import java.util.jar.Manifest
+
 
 class ScanHome:AppCompatActivity(),QRCodeView.Delegate {
-    override fun onScanQRCodeSuccess(result: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onCameraAmbientBrightnessChanged(isDark: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onScanQRCodeOpenCameraError() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity_scan)
@@ -32,4 +31,56 @@ class ScanHome:AppCompatActivity(),QRCodeView.Delegate {
         }
         zx_scan.setDelegate(this)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        zx_scan.startCamera() // 打开后置摄像头开始预览，但是并未开始识别
+        //        mZXingView.startCamera(Camera.CameraInfo.CAMERA_FACING_FRONT); // 打开前置摄像头开始预览，但是并未开始识别
+        zx_scan.setType(BarcodeType.TWO_DIMENSION, null) // 只识别二维条码
+        zx_scan.startSpotAndShowRect() // 显示扫描框，并开始识别
+    }
+
+    override fun onStop() {
+        zx_scan.stopCamera() // 关闭摄像头预览，并且隐藏扫描框
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        zx_scan.onDestroy() // 销毁二维码扫描控件
+        super.onDestroy()
+    }
+
+    private fun vibrate() {
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        vibrator.vibrate(200L)
+    }
+
+    override fun onScanQRCodeSuccess(result: String) {
+        title = "扫描结果为：$result"
+        vibrate()
+        Log.d("scanResult",result)
+        zx_scan.startSpot() // 开始识别
+    }
+
+    override fun onCameraAmbientBrightnessChanged(isDark: Boolean) {
+        // 这里是通过修改提示文案来展示环境是否过暗的状态，接入方也可以根据 isDark 的值来实现其他交互效果
+        var tipText = zx_scan.scanBoxView.tipText
+        val ambientBrightnessTip = "\n环境过暗，请打开闪光灯"
+        if (isDark) {
+            if (!tipText.contains(ambientBrightnessTip)) {
+                zx_scan.scanBoxView.tipText = tipText + ambientBrightnessTip
+            }
+        } else {
+            if (tipText.contains(ambientBrightnessTip)) {
+                tipText = tipText.substring(0, tipText.indexOf(ambientBrightnessTip))
+                zx_scan.scanBoxView.tipText = tipText
+            }
+        }
+    }
+
+    override fun onScanQRCodeOpenCameraError() {
+        Toast.makeText(this,"打开相机出错,检查相机权限.",Toast.LENGTH_LONG).show()
+    }
+
 }
