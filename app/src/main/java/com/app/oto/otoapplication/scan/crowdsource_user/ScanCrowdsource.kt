56b87f60988eb27f70d562d103c5535ed70c4433
zk <272991962@qq.com>
2019-08-17 12:26:04 +0800

    save

---
+++ app/src/main/java/com/app/oto/otoapplication/scan/crowdsource_user/ScanCrowdsource.kt
@@ -0,0 +1,11 @@
+package com.app.oto.otoapplication.scan.crowdsource_user
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ScanCrowdsource:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
