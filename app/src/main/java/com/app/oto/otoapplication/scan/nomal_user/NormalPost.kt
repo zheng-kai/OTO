3aed189c41ec0b0b4724138f6acb069d5835680d
zk <272991962@qq.com>
2019-08-17 17:26:05 +0800

    save

---
+++ app/src/main/java/com/app/oto/otoapplication/scan/nomal_user/NormalPost.kt
@@ -0,0 +1,11 @@
+package com.app.oto.otoapplication.scan.nomal_user
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NormalPost:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
