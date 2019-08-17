0fc52de12640bca69d56dea5a595dd313157cab9
zk <272991962@qq.com>
2019-08-17 19:54:18 +0800

    支付UI

--- app/src/main/java/com/app/oto/otoapplication/scan/nomal_user/NormalPost.kt
+++ app/src/main/java/com/app/oto/otoapplication/scan/nomal_user/NormalPost.kt
@@ -2,6 +2,7 @@ package com.app.oto.otoapplication.scan.nomal_user
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
@@ -10,6 +11,7 @@ import cn.edu.twt.retrox.recyclerviewdsl.withItems
 import com.app.oto.otoapplication.R
 import com.app.oto.otoapplication.scan.add
 import com.app.oto.otoapplication.transport.add
+import kotlinx.android.synthetic.main.navigation_layout.*
 import kotlinx.android.synthetic.main.scan_result_normal_post.*
 import org.jetbrains.anko.startActivity
 
@@ -18,7 +20,12 @@ class NormalPost:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.scan_result_normal_post)
         val anim = AnimationUtils.loadAnimation(this,R.anim.dialog_top_in)
-
+        scan_normal_post_navigation.apply {
+            btn_home.background = ResourcesCompat.getDrawable(resources, R.mipmap.home_clicked, null)
+        }
+        img_post_back.setOnClickListener {
+            onBackPressed()
+        }
         rec_normal_post.layoutManager = LinearLayoutManager(this)
         rec_normal_post.withItems {
             for(i in 1..3){
