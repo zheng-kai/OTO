12b8aa5b2c0e30d505858cf5aae145353c613fc7
zk <272991962@qq.com>
2019-08-24 20:00:31 +0800

    save

--- app/src/main/java/com/app/oto/otoapplication/commons/functions.kt
+++ app/src/main/java/com/app/oto/otoapplication/commons/functions.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.view.View
 import com.app.oto.otoapplication.OTOActivity
 import kotlinx.android.synthetic.main.navigation_layout.view.*
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Observer
 
 fun View.setNavigationClickListener(packageContext: Context){
     val intent = Intent(packageContext, OTOActivity::class.java)
@@ -20,3 +23,11 @@ fun View.setNavigationClickListener(packageContext: Context){
         packageContext.startActivity(intent)
     }
 }
+
+
+/**
+ * Created by SGXM on 2018/7/31.
+ */
+inline fun <T> LiveData<T>.bindNonNull(lifecycleOwner: LifecycleOwner, crossinline block: (T) -> Unit) =
+    observe(lifecycleOwner, Observer {
+        it?.let(block) })
