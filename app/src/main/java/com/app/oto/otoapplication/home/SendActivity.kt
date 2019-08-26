4600ceb34ace57e76417f3576ef568a876073fc2
haowenyu <1605349562@qq.com>
2019-08-26 15:44:25 +0800

    差下栏和上面返回的点击事件

--- app/src/main/java/com/app/oto/otoapplication/home/SendActivity.kt
+++ app/src/main/java/com/app/oto/otoapplication/home/SendActivity.kt
@@ -1,8 +1,10 @@
 package com.app.oto.otoapplication.home
 
 import android.os.Bundle
+import android.widget.TextView
 import com.app.oto.otoapplication.R
 import com.app.oto.otoapplication.personal.base.BaseActivity
+import com.app.oto.otoapplication.personal.base.getMyIntent
 
 class SendActivity:BaseActivity() {
 
@@ -13,7 +15,9 @@ class SendActivity:BaseActivity() {
 
 
     override fun initView() {
-
+    findViewById<TextView>(R.id.send_confirm).setOnClickListener {
+        startActivity(getMyIntent(this,senddetailActivity::class.java))
+    }
     }
 
     override fun initToolbar() {
--- app/src/main/java/com/app/oto/otoapplication/home/senddetailActivity.kt
+++ app/src/main/java/com/app/oto/otoapplication/home/senddetailActivity.kt
@@ -1,8 +1,14 @@
 package com.app.oto.otoapplication.home
 
+import android.os.Bundle
+import com.app.oto.otoapplication.R
 import com.app.oto.otoapplication.personal.base.BaseActivity
 
 class senddetailActivity: BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_deal)
+        super.onCreate(savedInstanceState)
+    }
     override fun initView() {
 
     }
